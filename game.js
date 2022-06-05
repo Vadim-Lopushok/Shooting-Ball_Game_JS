5757a3e219aa0b8977f37cda83bd3c45be767ba1
mighty <mighty2815736@gmail.com>
2022-06-05 14:19:49 +0300

    project setup

---
+++ game.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+console.log(context);
