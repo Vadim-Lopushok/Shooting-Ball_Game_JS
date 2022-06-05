79525e75297b4817461f2fab3522191d78a0f519
mighty <mighty2815736@gmail.com>
2022-06-05 14:37:27 +0300

    create a player

--- Player.js
+++ Player.js
@@ -6,4 +6,24 @@ const context = canvas.getContext('2d')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-console.log(context);
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+}
+
+const player = new Player(x, y, 30, 'blue');
+player.draw();
